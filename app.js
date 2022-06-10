ca9ddfe9de1fd3b101c94d3cb7d7189246d89b34
Clara <claraacevedo400@gmail.com>
2022-06-10 17:22:24 -0500

    Proyecto terminado

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+const publicPath= path.resolve(__dirname, './public')
+app.use(express.static(publicPath));
+app.get('/', (req, res)=>{
+    res.sendFile (path.join(__dirname,'/views/home.html'))
+});
+app.get('/register', (_req, res)=>{
+    res.sendFile (path.join(__dirname,'/views/register.html'))
+});
+app.get('/login', (_req, res)=>{
+    res.sendFile (path.join(__dirname,'/views/login.html'))
+});
+app.listen (3030, (req, res)=> console.log ('Servidor 3030 funcionando'));
